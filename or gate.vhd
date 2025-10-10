5f717d96ac487064a5e66496be2373c001a35871
AMAN MISHRA <amanmishaar@gmail.com>
2025-10-10 22:52:21 +0530

    Add files via upload

---
+++ or gate.vhd
@@ -0,0 +1,15 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity or_gate is
+    port (
+        A, B : in  STD_LOGIC;
+        Y    : out STD_LOGIC
+    );
+end or_gate;
+
+architecture behavior of or_gate is
+begin
+    Y <= A OR B;
+end behavior;
+
